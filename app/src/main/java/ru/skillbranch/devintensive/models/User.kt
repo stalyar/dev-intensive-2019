6c3524e6735c01bb280e31c2ff825ccd5c213af7
Stalyar <stalyar777@gmail.com>
2020-02-06 23:15:10 +0300

    hometask_2 stage 1 done

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -10,29 +10,16 @@ data class User (
     var avatar:String?,
     var rating:Int = 0,
     var respect:Int = 0,
-    val lastVisit:Date? = null,
+    val lastVisit:Date? = Date(),
     val isOnline:Boolean = false
 ){
-    constructor(id: String, firstName: String?, lastName: String?) : this(
-        id = id,
-        firstName = firstName,
-        lastName = lastName,
-        avatar = null
-    )
-
-    constructor(id: String) : this(id, "John", "Doe")
-
-    init {
-        println("It's Alive!!! \n" +
-                "${if (lastName==="Doe") "His name id $firstName $lastName" else "And his name id $firstName $lastName"}\n")
-    }
 
     companion object Factory{
         private var lastId = -1
         fun makeUser(fullName:String?) : User{
             lastId++
             val (firstName, lastName) = Utils.parseFullName(fullName)
-            return User("$lastId", firstName = firstName, lastName = lastName)
+            return User("$lastId", firstName = firstName, lastName = lastName, avatar = null)
         }
     }
 }
