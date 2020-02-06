c1d56b033dc1a77137eae76f8d9c1f646a7b05d3
Stalyar <stalyar777@gmail.com>
2020-02-06 23:23:25 +0300

    hometask_2 stage 2 done

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -10,7 +10,7 @@ class TextMessage(
     isIncoming: Boolean  = false,
     date: Date = Date(),
     val text: String?)
-    : BaseMessage(id, from,chat, isIncoming, date) {
+    : BaseMessage(id, from, chat, isIncoming, date) {
     override fun formatMessage(): String = "id:$id ${from?.firstName}" +
             " ${if(isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
