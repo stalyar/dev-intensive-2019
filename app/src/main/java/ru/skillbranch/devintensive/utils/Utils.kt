cd818fcd6c7b975d9e838b82324e319025000df7
Stalyar <stalyar777@gmail.com>
2020-02-08 01:58:38 +0300

    hometask_2 stage 7 done

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,8 @@
 package ru.skillbranch.devintensive.utils
 
+import java.util.*
+import kotlin.text.StringBuilder
+
 object Utils {
 
     fun parseFullName(fullName : String?) : Pair<String?, String?>{
@@ -10,9 +13,68 @@ object Utils {
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        //TODO
-        return " "
+    fun transliteration(payload: String?, divider: String = " "): String {
+
+        val (firstName, lastName) = parseFullName(payload)
+
+        var resRu:String? = if (firstName!=null && lastName!=null) "$firstName$divider$lastName"
+        else if (firstName!=null && lastName==null) "$firstName"
+        else if (firstName==null && lastName!=null) "$lastName"
+        else null
+
+        val resEn = StringBuilder()
+        var letterEn = ""
+
+        if (resRu!=null){
+            resRu=resRu.toLowerCase()
+            var isDivider = false
+            for(letter in resRu){
+                letterEn = when (letter){
+                    'а' -> "a"
+                    'б' -> "b"
+                    'в' -> "v"
+                    'г' -> "g"
+                    'д' -> "d"
+                    'е' -> "e"
+                    'ё' -> "e"
+                    'ж' -> "zh"
+                    'з' -> "z"
+                    'и' -> "i"
+                    'й' -> "i"
+                    'к' -> "k"
+                    'л' -> "l"
+                    'м' -> "m"
+                    'н' -> "n"
+                    'о' -> "o"
+                    'п' -> "p"
+                    'р' -> "r"
+                    'с' -> "s"
+                    'т' -> "t"
+                    'у' -> "u"
+                    'ф' -> "f"
+                    'х' -> "h"
+                    'ц' -> "c"
+                    'ч' -> "ch"
+                    'ш' -> "sh"
+                    'щ' -> "sh'"
+                    'ъ' -> ""
+                    'ы' -> "i"
+                    'ь' -> ""
+                    'э' -> "e"
+                    'ю' -> "yu"
+                    'я' -> "ya"
+                    else -> "$letter"
+                }
+                if (isDivider) {
+                    letterEn=letterEn.capitalize()
+                    isDivider=false
+                }
+                if (letterEn.equals(divider)) isDivider = true
+                resEn.append(letterEn)
+            }
+        }
+
+        return resEn.toString().capitalize()
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
