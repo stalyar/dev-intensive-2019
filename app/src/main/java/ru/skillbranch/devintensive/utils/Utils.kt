09dff79d60a9332ceaef9d81ce591787654b6b41
Stalyar <stalyar777@gmail.com>
2020-02-07 23:54:47 +0300

    hometask_2 stage 3 done

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,9 +4,9 @@ object Utils {
 
     fun parseFullName(fullName : String?) : Pair<String?, String?>{
         //TODO FIX ME
-        val parts : List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val parts : List<String>? = fullName?.split(" ", limit=2)
+        val firstName = if (parts?.getOrNull(0)=="") null else parts?.getOrNull(0)
+        val lastName = if (parts?.getOrNull(1)=="") null else parts?.getOrNull(1)
         //return Pair(firstName, lastName)
         return firstName to lastName
     }
