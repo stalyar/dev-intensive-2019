13ae42aa811279e1b35f6a100ee291fb58152b56
Stalyar <stalyar777@gmail.com>
2020-02-08 00:53:17 +0300

    hometask_2 stage 6 done

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,7 +3,6 @@ package ru.skillbranch.devintensive.utils
 object Utils {
 
     fun parseFullName(fullName : String?) : Pair<String?, String?>{
-        //TODO FIX ME
         val parts : List<String>? = fullName?.split(" ", limit=2)
         val firstName = if (parts?.getOrNull(0)=="") null else parts?.getOrNull(0)
         val lastName = if (parts?.getOrNull(1)=="") null else parts?.getOrNull(1)
@@ -17,7 +16,19 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        //TODO
-        return ""
+        val firstInitial:Char? = when (firstName){
+            ""," ", null -> null
+            else -> firstName?.first()?.toUpperCase()
+        }
+        val lastInitial:Char? = when (lastName){
+            ""," ", null -> null
+            else -> lastName?.first()?.toUpperCase()
+        }
+        val res:String? = if (firstInitial!=null && lastInitial!=null) "$firstInitial$lastInitial"
+                            else if (firstInitial!=null && lastInitial==null) "$firstInitial"
+                            else if (firstInitial==null && lastInitial!=null) "$lastInitial"
+                            else null
+
+        return res
     }
 }
