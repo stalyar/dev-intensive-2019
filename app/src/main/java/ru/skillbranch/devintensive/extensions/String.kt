a3d6647f5cfb85f468a397a6d5a617f8e4485be5
Stalyar <stalyar777@gmail.com>
2020-02-08 23:26:49 +0300

    hometask_2 stage 9 done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(value: Int = 16):String{
+    if (this.trimEnd().length < value) return this.trimEnd()
+    var mes = this.subSequence(0, value)
+    mes = mes.trimEnd()
+    mes = "$mes..."
+    return mes
+}
--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -22,4 +22,31 @@ data class User (
             return User("$lastId", firstName = firstName, lastName = lastName, avatar = null)
         }
     }
+
+    private constructor(builder: Builder) : this(builder.id, builder.firstName, builder.lastName,
+                                                    builder.avatar, builder.respect, builder.rating,
+                                                    builder.lastVisit, builder.isOnline)
+
+    class Builder{
+        var id:String = ""
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String = ""
+        var rating:Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
+
+        fun id(id:String) = apply { this.id = id }
+        fun firstName(firstName:String?) = apply { this.firstName = firstName }
+        fun lastName(lastName:String?) = apply { this.lastName = lastName }
+        fun avatar(avatar:String) = apply { this.avatar = avatar }
+        fun rating(rating:Int) = apply { this.rating = rating }
+        fun respect(respect:Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit:Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline:Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = User(this)
+    }
+
 }
