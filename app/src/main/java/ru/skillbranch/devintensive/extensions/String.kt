aa93fcc1d38e0832b36ab9599836c57ff8e59385
Stalyar <stalyar777@gmail.com>
2020-02-10 01:32:02 +0300

    hometask_2 all done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,9 +1,59 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.StringBuilder
+
 fun String.truncate(value: Int = 16):String{
     if (this.trimEnd().length < value) return this.trimEnd()
-    var mes = this.subSequence(0, value)
-    mes = mes.trimEnd()
-    mes = "$mes..."
-    return mes
+    var res = this.subSequence(0, value)
+    res = res.trimEnd()
+    res = "$res..."
+    return res
+}
+
+fun String.stripHtml():String{
+    var res = StringBuilder("")
+    //remove odd spaces
+    var isFirstSpace = false
+    for (char in this){
+        if (char==' '){
+            if (isFirstSpace){
+                continue
+            }
+            else {
+                isFirstSpace = true
+                res.append(char)
+            }
+        }
+        else {
+            isFirstSpace = false
+            res.append(char)
+        }
+    }
+    //remove <tag>
+    val stringNoOddSpaces = res.toString()
+    res = StringBuilder("")
+    var isLessSign = false
+    for (char in stringNoOddSpaces){
+        if (char=='<'){
+            isLessSign = true
+            continue
+        }
+        else if (char=='>' && isLessSign){
+            isLessSign=false
+            continue
+        }
+        else {
+            if (isLessSign) continue
+            res.append(char)
+        }
+    }
+    //remove escape sings
+    var stringNoTags = res.toString()
+    stringNoTags = stringNoTags.replace("&amp;", "&")
+    stringNoTags = stringNoTags.replace("&lt;", "<")
+    stringNoTags = stringNoTags.replace("&gt;", ">")
+    stringNoTags = stringNoTags.replace("&rsquo;", "'")
+    stringNoTags = stringNoTags.replace("&quot;", "\"")
+
+    return stringNoTags
 }
